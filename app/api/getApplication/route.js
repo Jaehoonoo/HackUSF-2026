2a7efde58f5294fe1ffc40ce415984a27b4ed740
unknown <nbpntp@gmail.com>
2026-01-02 12:44:16 -0500

    Changed response method to NextResponse

--- app/api/getApplication/route.js
+++ app/api/getApplication/route.js
@@ -1,5 +1,6 @@
 import {doc, getDoc} from "firebase/firestore";
 import {db} from "@/firebase";
+import { NextResponse } from "next/server";
 
 export async function GET(req) {
     try {
@@ -8,13 +9,13 @@ export async function GET(req) {
         const docSnap = await getDoc(docRef)
 
         if (docSnap.exists()) {
-          return new Response(JSON.stringify({success: true, data: docSnap.data()}), {status: 200})
+          return NextResponse.json({success: true, data: docSnap.data()}, {status: 200})
         } else {
-          return new Response(JSON.stringify({success: false, error: "Cannot find user"}), {status: 404})
+          return NextResponse.json({success: false, error: "Cannot find user"}, {status: 404})
         }
 
     } catch (error) {
         console.error(error)
-        return new Response(JSON.stringify({success: false, error: error.message}), {status: 500})
+        return NextResponse.json({success: false, error: error.message}, {status: 500})
     }
 }
