582cf80798048661cbbfefcba27b4659529e12fb
lghuy05 <luonggiahuy1909@gmail.com>
2026-01-03 14:26:04 -0500

    schemas

--- app/api/createProfile/route.js
+++ app/api/createProfile/route.js
@@ -1,64 +1,64 @@
-import {doc, getDoc, setDoc} from "firebase/firestore";
-import {db} from "@/firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "@/firebase";
 
-export async function POST(req){
-    try{
+export async function POST(req) {
+  try {
 
-        // Get the JSON data from the frontend (should include userId and possibly other fields)
-        const data = await req.json();
-        // Create reference to the specific user documetn in Firestore using the userId we got from the request 
-        const userDocRef = doc(db, "users", data.userId);
-        // Fetch user actual information from Firestore, this will allow us to check if the user already exists
-        const userSnap = await getDoc(userDocRef);
+    // Get the JSON data from the frontend (should include userId and possibly other fields)
+    const data = await req.json();
+    // Create reference to the specific user documetn in Firestore using the userId we got from the request 
+    const userDocRef = doc(db, "users", data.userId);
+    // Fetch user actual information from Firestore, this will allow us to check if the user already exists
+    const userSnap = await getDoc(userDocRef);
 
-        if(userSnap.exists()){
-            
-            // If user already exists, return a early response with success message
-            return new Response(JSON.stringify({success: true, message: "Profile already exists"}),{status: 200})
-        }
-        else{
+    if (userSnap.exists()) {
 
-            // If user doesnt exist we create a new document with default fields
-            // setDoc writes the document to Firestore
-            await setDoc(userDocRef, {
-
-                age: "",
-                checkedIn: false,
-                codeOfConduct: true,
-                country: "",
-                dietaryRestrictions: [],
-                disclaimer: false,
-                email: "",
-                ethnicity: "",
-                fileName: "",
-                firstHackathon: false,
-                firstName: "",
-                gender: "",
-                lastName: "",
-                levelOfStudy: "",
-                major: "",
-                meal: "",
-                notifications: false,
-                otherAccomodations: "",
-                otherSchool: "",
-                phone: "",
-                privacyPolicy: true,
-                resumeURL: "",
-                rsvp: false,
-                school : "",
-                shirtSize: "",
-                status: "",
-            })
-        }
-        // After crating the profile sends a success response to frontend
-        return new Response(JSON.stringify({success: true, message : "Profile created succesfully"}), {status:200})
-
+      // If user already exists, return a early response with success message
+      return new Response(JSON.stringify({ success: true, message: "Profile already exists" }), { status: 200 })
     }
-    catch(error){
-        // Log error to the backend console
-        console.error(error);
-        // Return error response to the frotend with error message
-        return new Response(JSON.stringify({success: false, error: error.message}) , {status : 500})
+    else {
 
+      // If user doesnt exist we create a new document with default fields
+      // setDoc writes the document to Firestore
+      await setDoc(userDocRef, {
+
+        age: "",
+        checkedIn: false,
+        codeOfConduct: true,
+        country: "",
+        dietaryRestrictions: [],
+        disclaimer: false,
+        email: "",
+        ethnicity: "",
+        fileName: "",
+        numHackathons: 0,
+        firstName: "",
+        gender: "",
+        gradYear: "",
+        lastName: "",
+        levelOfStudy: "",
+        major: "",
+        notifications: false,
+        otherAccomodations: "",
+        otherSchool: "",
+        phone: "",
+        privacyPolicy: true,
+        resumeURL: "",
+        rsvp: false,
+        school: "",
+        shirtSize: "",
+        status: "",
+      })
     }
+    // After crating the profile sends a success response to frontend
+    return new Response(JSON.stringify({ success: true, message: "Profile created succesfully" }), { status: 200 })
+
+  }
+  catch (error) {
+    // Log error to the backend console
+    console.error(error);
+    // Return error response to the frotend with error message
+    return new Response(JSON.stringify({ success: false, error: error.message }), { status: 500 })
+
+  }
 }
