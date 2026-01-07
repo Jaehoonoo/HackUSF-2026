33190c70687aa17d3e111110a86508417a29c1a4
Ayush Pramanik <ayushpramanik2007@gmail.com>
2026-01-07 13:54:22 +0700

    Added about section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 "use client";
 
 import Header from "@/components/header/page";
+import About from "@/components/about/page";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <About />
     </div>
   );
 }
