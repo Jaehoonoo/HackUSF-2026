a7110ab642099791efe8ec107473bac056c69fa4
Jaehoonoo <justjhoon@gmail.com>
2026-01-29 15:08:26 -0500

    slight textshadow change

--- components/faq/page.js
+++ components/faq/page.js
@@ -253,7 +253,7 @@ export default function FAQ() {
               lineHeight: "100%",
               letterSpacing: 0,
               color: "#fcf0da",
-              textShadow: "1px 2px 2px rgba(0, 0, 0, 0.5)",
+              textShadow: "1px 1px 2px rgba(0, 0, 0, 0.5)",
             }}
           >
             FAQ
